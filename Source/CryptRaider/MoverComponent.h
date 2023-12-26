fc1d37a2011a9ff841abeb2de0dc159407d11265
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-12-25 20:53:11 -0500

    imp mover component to move walls on action

---
+++ Source/CryptRaider/MoverComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MoverComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UMoverComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMoverComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere)
+	FVector MovingOffset;
+	
+	UPROPERTY(EditAnywhere)
+	float duration;
+
+	UPROPERTY(EditAnywhere)
+	bool move=false;
+
+	FVector startLocation;
+};
